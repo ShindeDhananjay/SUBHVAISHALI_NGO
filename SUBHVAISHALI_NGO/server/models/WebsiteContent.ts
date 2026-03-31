2814f39f946ad275bde59827ef22572efd769056
Dhananjay Shinde <sdhananjay1299@gmail.com>
2026-03-31 18:00:20 +0530

    Delete SUBHVAISHALI_NGO directory

--- SUBHVAISHALI_NGO/server/models/WebsiteContent.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const websiteContentSchema = new mongoose.Schema({
-  key: { type: String, required: true, unique: true }, // e.g., 'homepage_hero', 'about_mission'
-  value: { type: mongoose.Schema.Types.Mixed, required: true }, // can be string, object, array
-}, { timestamps: true });
-
-export const WebsiteContent = mongoose.model("WebsiteContent", websiteContentSchema);
